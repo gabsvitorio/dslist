598bd9e5d47df48f9b49eea59f8c6394af3b1f80
Ghost Host <gaby18vsilva@gmail.com>
2023-05-09 21:20:07 -0400

    GET game by id

--- src/main/java/com/gabsvitorio/dslist/controllers/GameController.java
+++ src/main/java/com/gabsvitorio/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.gabsvitorio.dslist.controllers;
 
+import com.gabsvitorio.dslist.dto.GameDTO;
 import com.gabsvitorio.dslist.dto.GameMinDTO;
 import com.gabsvitorio.dslist.entities.Game;
 import com.gabsvitorio.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,12 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
         return result;
     }
 }
